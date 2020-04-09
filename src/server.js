e9a3fdaaef2482d46971de95705b20887621bb83
Eric de Moraes <eric.guedes.sm@gmail.com>
2020-04-09 20:17:57 -0300

    resolving import problems

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
 import app from './app';
 
-app.listen(process.env.port || 3333);
-console.log('Server up and running');
+app.listen(process.env.PORT || 3333);
+console.log(`Server up and running ${process.env.PORT || 3333}`);
